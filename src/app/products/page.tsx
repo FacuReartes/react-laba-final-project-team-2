beba8a1796bc94a4924dad540a30ae8c028f5c9e
Lucas Montecino <lucasmontecino95@gmail.com>
2024-08-22 13:50:49 -0300

    create: products page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from "@mui/material";
+
+export default function ProductsPage() {
+  return (
+    <Container sx={{ border: "1px solid #ff0000", minHeight: "100vh", my: 2 }}>
+      <Typography variant="h1">This is the Products Page</Typography>
+    </Container>
+  );
+}
