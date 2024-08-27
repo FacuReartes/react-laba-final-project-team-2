f71f37c0c7827b2ae7892c52557d5581d3e86d7c
pinoen <pinoen@yahoo.com.ar>
2024-08-27 11:23:09 -0300

    feature: settings page

---
+++ src/app/(main)/profile/settings/page.tsx
@@ -0,0 +1,10 @@
+import SettingsForm from "@/components/SettingsForm";
+import { Box } from "@mui/material";
+
+export default function Page() {
+  return (
+    <Box>
+      <SettingsForm />
+    </Box>
+  )
+}
