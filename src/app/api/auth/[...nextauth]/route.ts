f1f7cb272b823cd81fd3be254deeb4ded7c5b097
facureartes <facureartes9@gmail.com>
2024-08-30 18:02:08 -0300

    [SPT2-92] chore: installed next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,40 @@
+import { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? ''
+    }),
+  CredentialsProvider({
+    name: "Credentials",
+    credentials: {
+      username: { label: "Username", type: "text" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      // we receive the user data, with this data we can make the API call
+      const { username, password } = credentials as any;
+      // Mock user
+      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+      if (user) {
+        // Any object returned will be saved in `user` property of the JWT
+        return user
+      } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      }
+    }
+  })
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
