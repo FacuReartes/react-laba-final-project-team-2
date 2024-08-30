1cd687c32664c23e891cf1801cb47de6e4ffbd27
pinoen <pinoen@yahoo.com.ar>
2024-08-30 14:43:19 -0300

    [SPT2-20-21] feat: add validation in reset password form

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,17 +1,27 @@
-import { Box } from "@mui/material";
-import Logo from "@/components/common/Logo";
-import ResetPasswordForm from "@/components/auth/resetPassword/ResetPasswordForm";
-import Image from "next/image";
+import { Box } from '@mui/material';
+import Logo from '@/components/common/Logo';
+import ResetPasswordForm from '@/components/auth/resetPassword/ResetPasswordForm';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-  <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-    <Logo />
-    <ResetPasswordForm />
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Logo />
+      <ResetPasswordForm />
 
-     <Box sx={{ position: {md: 'relative'}, display: {md: 'flex', xs: 'none'} }}>
-        <Image src="/sneakers-reset-password.svg" alt="Sneakers" width={961} height={962} />
+      <Box
+        sx={{
+          position: { md: 'relative' },
+          display: { md: 'flex', xs: 'none' },
+        }}
+      >
+        <Image
+          src="/sneakers-reset-password.svg"
+          alt="Sneakers"
+          width={961}
+          height={962}
+        />
       </Box>
-  </Box>
-  )
+    </Box>
+  );
 }
