35a860279326dee76cdd8a959fa526c6605ea530
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-08-30 15:04:55 -0600

    [SPT2-5] feat: Password input validation

--- src/lib/schemas/authSchemas.ts
+++ src/lib/schemas/authSchemas.ts
@@ -1,5 +1,24 @@
 import { z } from 'zod';
 
 export const signInSchema = z.object({
-  email: z.string().min(1, { message: 'Email is required'}).email({ message: 'Invalid email address' })
+  email: z
+    .string()
+    .min(1, { message: 'Email is required' })
+    .email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(1, { message: 'Password is required' })
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .max(50, { message: 'Password must be less than 50 characters long' })
+    .regex(/[A-Z]/, {
+      message: 'Password must contain at least one uppercase letter',
+    })
+    .regex(/[a-z]/, {
+      message: 'Password must contain at least one lowercase letter',
+    })
+    .regex(/\d/, { message: 'Password must contain at least one number' })
+    .regex(/[@$!%*?&]/, {
+      message: 'Password must contain at least one special character: @$!%*?&',
+    })
+    .regex(/^\S*$/, { message: 'Password cannot contain spaces' }),
 });
