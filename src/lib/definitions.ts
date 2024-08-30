cb7e856566df111f5805c9e2a77afeab6f23f184
pinoen <pinoen@yahoo.com.ar>
2024-08-30 16:44:09 -0300

    [SPT2-2] chore: add types for settings, signup and reset password forms

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -6,3 +6,22 @@ export type ProductType = {
   price: number;
   gender: 'Man' | 'Woman' | 'Unisex';
 };
+
+export type SettingsFormData = {
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+};
+
+export type SignUpFormData = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export type ResetPasswordFormData = {
+  password: string;
+  confirmPassword: string;
+};
