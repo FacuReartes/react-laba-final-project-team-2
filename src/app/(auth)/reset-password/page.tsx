641ddf03f88597c69336b93135881dbb31c6660c
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-02 16:58:18 -0300

    [SPT2-103] style(auth): fixing sizes of images in sign in sign up and reset password

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -5,13 +5,35 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-  <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-    <Logo />
-    <ResetPasswordForm />
-
-     <Box sx={{ position: {md: 'relative'}, display: {md: 'flex', xs: 'none'} }}>
-        <Image src="/sneakers-reset-password.svg" alt="Sneakers" width={961} height={962} />
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        width: { md: '100%' },
+        height: { md: '100vh' },
+        overflow: 'hidden',
+      }}
+    >
+      <Box sx={{ display: 'flex', justifyContent: 'center', flex: 1 }}>
+        <Logo />
+        <ResetPasswordForm />
       </Box>
-  </Box>
-  )
+
+      <Box
+        sx={{
+          position: { md: 'relative' },
+          display: { md: 'flex', xs: 'none' },
+          flex: 1,
+        }}
+      >
+        <Image
+          src="/sneakers-reset-password.svg"
+          alt="Sneakers"
+          width={961}
+          height={962}
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      </Box>
+    </Box>
+  );
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -4,16 +4,44 @@ import Logo from '@/components/common/Logo';
 import { Box } from '@mui/material';
 import Image from 'next/image';
 
-export default function Page() {  
+export default function Page() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-      <Logo />
-      <SignupForm />
-      
-      <Box sx={{ position: {md: 'relative'}, display: {md: 'flex', xs: 'none'} }}>
-        <Image src="/sneakers-signup.svg" alt="Sneakers" width={961} height={962} />
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: { md: '100vh' },
+        overflow: 'hidden',
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Logo />
+        <SignupForm />
+      </Box>
+
+      <Box
+        sx={{
+          position: { md: 'relative' },
+          display: { xl: 'flex', xs: 'none' },
+          flex: 1,
+        }}
+      >
+        <Image
+          src="/sneakers-signup.svg"
+          alt="Sneakers"
+          width={961}
+          height={962}
+          style={{ width: '100%', objectFit: 'cover', height: '100%' }}
+        />
         <SignupCard />
       </Box>
     </Box>
-  )
+  );
 }
