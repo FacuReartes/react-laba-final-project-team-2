009111b12122b91bededbc05ef7aca611beac914
pinoen <pinoen@yahoo.com.ar>
2024-09-02 21:18:38 -0300

    [SPT2-18] style(SignUpForm): adjust gap between input fields

--- src/components/auth/signUp/SignupForm.tsx
+++ src/components/auth/signUp/SignupForm.tsx
@@ -34,7 +34,7 @@ const SignupForm = () => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        pt: isDesktop ? '288px' : '94px',
+        pt: isDesktop ? '208px' : '94px',
         bgcolor: '#fff',
       }}
     >
@@ -65,14 +65,12 @@ const SignupForm = () => {
           {...register('name')}
           error={Boolean(errors.name)}
           helperText={errors.name ? errors.name.message : ''}
-          sx={errors.name ? { height: '48px', mb: '16px' } : { height: '48px', mb: '0' }}
         />
         <TextField
           id="outlined-basic"
           label="Email *"
           variant="outlined"
           placeholder="example@mail.com"
-          sx={errors.name ? { height: '48px', mb: '16px' } : { height: '48px', mb: '0' }}
           {...register('email')}
           error={Boolean(errors.email)}
           helperText={errors.email ? errors.email.message : ''}
@@ -83,7 +81,6 @@ const SignupForm = () => {
           label="Password *"
           variant="outlined"
           placeholder="at least 8 characters"
-          sx={errors.name ? { height: '48px', mb: '16px' } : { height: '48px', mb: '0' }}
           {...register('password')}
           error={Boolean(errors.password)}
           helperText={errors.password ? errors.password.message : ''}
@@ -94,10 +91,11 @@ const SignupForm = () => {
           label="Confirm Password *"
           variant="outlined"
           placeholder="at least 8 characters"
-          sx={errors.name ? { height: '48px', mb: '16px' } : { height: '48px', mb: '0' }}
           {...register('confirmPassword')}
           error={Boolean(errors.confirmPassword)}
-          helperText={errors.confirmPassword ? errors.confirmPassword.message : ''}
+          helperText={
+            errors.confirmPassword ? errors.confirmPassword.message : ''
+          }
         />
 
         <Box
