5a75d675073bfaa8a13954018b7e58ffc7d1d621
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-05 23:52:36 -0300

    [SPT2-112] fix(products-view): creating new component Product Detail and create mock object

--- src/components/profile/ProductDetail.tsx
+++ src/components/profile/ProductDetail.tsx
@@ -1,6 +1,18 @@
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 export default function ProductDetail({ id }: { id: string | number }) {
+  const productDetail = {
+    title: 'Nike Air Max 270',
+    price: '160',
+    genre: 'Womens shoes',
+    description:
+      'Boasting the first-ever Max Air unit created specifically for Nike Sportswear, the Nike Air Max 270 delivers an Air unit that absorbs and gives back energy with every springy step. Updated for modern comfort, it nods to the original, 1991 Air Max 180 with its exaggerated tongue top and heritage tongue logo.',
+  };
+
   console.log(id);
-  return <Container></Container>;
+  return (
+    <Container>
+      <Box></Box>
+    </Container>
+  );
 }
