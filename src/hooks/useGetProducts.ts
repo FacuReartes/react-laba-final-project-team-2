b291b5b8d5fc7dca0c1c9e87e25d52d63b170c59
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-09 19:56:49 -0300

    [SPT2-113] feat: typing the Product Type

--- src/hooks/useGetProducts.ts
+++ src/hooks/useGetProducts.ts
@@ -1,20 +1,30 @@
-import axios from 'axios';
+import { ProductType } from '@/lib/definitions';
+import axios, { AxiosError } from 'axios';
 
 export default function useGetProducts() {
   async function getProducts() {
-    const req = axios(
-      'https://shoes-shop-strapi.herokuapp.com/api/products?populate=*'
-    );
-    const res = (await req).data.data;
-    return res;
+    try {
+      const req = axios(
+        'https://shoes-shop-strapi.herokuapp.com/api/products?populate=*'
+      );
+      const res = (await req).data.data;
+      return res;
+    } catch (error: unknown) {
+      const knownError = error as AxiosError;
+      if (knownError.response) console.log(knownError.response.status);
+      else if (knownError.request) console.log(knownError.request);
+      else console.log('Error', knownError.message);
+    }
   }
 
   async function filterProducts() {
     const result = await getProducts();
     return result.filter(
-      (item: { id: string; attributes: { teamName: string } }) =>
-        item.attributes.teamName === 'team-2'
+      (item: ProductType) => item.attributes.teamName === 'team-2'
     );
+    // return result.filter(
+    //   (item: ProductType) => item.attributes.name === 'team-5'
+    // );
   }
 
   return { filterProducts };
