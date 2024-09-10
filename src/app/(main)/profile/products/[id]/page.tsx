0471cd93e8ae5e7176f82181bec5b9423818e1c7
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-10 16:51:22 -0300

    [SPT2-126] fix: deleting useless tsx for showing the id, because it was for development purposes

--- src/app/(main)/profile/products/[id]/page.tsx
+++ src/app/(main)/profile/products/[id]/page.tsx
@@ -1,10 +1,9 @@
 import ProductDetail from '@/components/profile/ProductDetail';
 import { Box } from '@mui/material';
 
-export default function Page({ params }: { params: { id: string | number } }) {
+export default function Page({ params }: { params: { id: number } }) {
   return (
     <Box sx={{ pb: 4 }}>
-      My id: {params.id}
       <ProductDetail id={params.id} />
     </Box>
   );
