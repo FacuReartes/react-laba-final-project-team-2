db053f49032cec1e5e3f573a5610ca6bafcf9e0f
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-10 19:57:08 -0300

    [SPT2-126] fix: adding some styles for the Products Page Container

--- src/app/(main)/profile/products/[id]/page.tsx
+++ src/app/(main)/profile/products/[id]/page.tsx
@@ -3,7 +3,14 @@ import { Box } from '@mui/material';
 
 export default function Page({ params }: { params: { id: number } }) {
   return (
-    <Box sx={{ pb: 4 }}>
+    <Box
+      sx={{
+        pb: 4,
+        display: 'flex',
+        ml: 1,
+        width: 1,
+      }}
+    >
       <ProductDetail id={params.id} />
     </Box>
   );
