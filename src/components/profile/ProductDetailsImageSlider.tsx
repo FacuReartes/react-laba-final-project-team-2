46db8c44f145cc5cf1c8dcd84b1edffa54422aaa
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-10 19:57:54 -0300

    [SPT2-126] fix: adding the image slider component

--- src/components/profile/ProductDetailsImageSlider.tsx
+++ src/components/profile/ProductDetailsImageSlider.tsx
@@ -1,6 +1,6 @@
 import { ImageType } from '@/lib/definitions';
 import { Box } from '@mui/material';
-import Image from 'next/image';
+import ImageSlider from './ImageSlider';
 
 export default function ProductDetailsImageSlider({
   imageUrls,
@@ -8,33 +8,14 @@ export default function ProductDetailsImageSlider({
   imageUrls: ImageType[];
 }) {
   return (
-    <Box sx={{ display: 'flex', gap: 1, height: '628px', width: '60%' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '76px',
-          gap: 2,
-        }}
-      >
-        {imageUrls.map(img => (
-          <Image
-            src={img.attributes.url}
-            alt={'product-img'}
-            key={img.attributes.name}
-            width={76}
-            height={76}
-            style={{ flexGrow: 1, objectFit: 'cover', height: '100%' }}
-          />
-        ))}
-      </Box>
-      <Image
-        src={imageUrls[0].attributes.url}
-        alt={imageUrls[0].attributes.name}
-        width={588}
-        height={628}
-        style={{ maxWidth: '588px', objectFit: 'cover', height: '100%' }}
-      />
+    <Box
+      sx={{
+        display: 'flex',
+        gap: 1,
+        height: '628px',
+      }}
+    >
+      <ImageSlider imageUrls={imageUrls} />
     </Box>
   );
 }
