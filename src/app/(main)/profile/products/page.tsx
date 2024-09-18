684fc4c363c76aa90f487942ffdd91d766d5e9ba
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-18 14:24:19 -0300

    [SPT2-135] fix(Products-page): deleting the hidratyon prefetch

--- src/app/(main)/profile/products/page.tsx
+++ src/app/(main)/profile/products/page.tsx
@@ -1,14 +1,5 @@
 import Products from '@/components/profile/Products';
-// import useGetProducts from '@/hooks/useGetProducts';
-// import {
-//   dehydrate,
-//   HydrationBoundary,
-//   QueryClient,
-// } from '@tanstack/react-query';
 
 export default async function ProductsPage() {
-  // const queryClient = new QueryClient();
-  // await queryClient.prefetchQuery(useGetProducts());
-
   return <Products />;
 }
