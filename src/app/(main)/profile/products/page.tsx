6a07f1fd733fee3631880a5d698fd31483ad155f
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-18 14:12:36 -0300

    [SPT2-135] fix(Products-page): refactor the server side page to make it client component

--- src/app/(main)/profile/products/page.tsx
+++ src/app/(main)/profile/products/page.tsx
@@ -1,18 +1,14 @@
 import Products from '@/components/profile/Products';
-import useGetProducts from '@/hooks/useGetProducts';
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query';
+// import useGetProducts from '@/hooks/useGetProducts';
+// import {
+//   dehydrate,
+//   HydrationBoundary,
+//   QueryClient,
+// } from '@tanstack/react-query';
 
 export default async function ProductsPage() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(useGetProducts());
+  // const queryClient = new QueryClient();
+  // await queryClient.prefetchQuery(useGetProducts());
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Products />
-    </HydrationBoundary>
-  );
+  return <Products />;
 }
