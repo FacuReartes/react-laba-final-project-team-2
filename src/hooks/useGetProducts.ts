8ee5ecd73bae669019d7ed483ab880b3ba618e3a
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-18 14:11:28 -0300

    [SPT2-135] fix(useGetProducts): added the headers authoritzation and the token and the userID

--- src/hooks/useGetProducts.ts
+++ src/hooks/useGetProducts.ts
@@ -1,9 +1,17 @@
-export default function useGetProducts() {
+export default function useGetProducts(
+  token: string | undefined,
+  userID: number | undefined
+) {
   const queryKey = ['products'];
 
   const queryFn = async () => {
     const req = await fetch(
-      'https://shoes-shop-strapi.herokuapp.com/api/products?filters[teamName]=team-2&populate=*'
+      `https://shoes-shop-strapi.herokuapp.com/api/products?filters[teamName]=team-2&filters[userID]=${userID}&populate=*`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
     const res = await req.json();
     return res.data;
