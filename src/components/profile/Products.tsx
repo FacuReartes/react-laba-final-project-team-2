458a76707c19c54275a6aaa77fd1ce915ca12075
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-18 14:12:05 -0300

    [SPT2-135] fix(Products): refactor the session data to use userID and token

--- src/components/profile/Products.tsx
+++ src/components/profile/Products.tsx
@@ -10,11 +10,16 @@ import { useQuery } from '@tanstack/react-query';
 import heroImage from '@/images/products-hero-img.webp';
 
 export default function Products() {
-  const session = useSession();
-  const { data: userData } = useUserData(session.data?.user.jwt);
+  const { data: session } = useSession();
+
+  const token = session?.user.jwt;
+  const userID = session?.user.user.id;
+
+  const { data: userData } = useUserData(token);
+
   const router = useRouter();
 
-  const { data: products } = useQuery(useGetProducts());
+  const { data: products } = useQuery(useGetProducts(token, userID));
 
   const isDesktop = useMediaQuery('(min-width: 700px)');
   type MockUser = {
