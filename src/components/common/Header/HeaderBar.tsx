9b07ae1973e7d48a936931d7acc3c9a069359dfb
facureartes <facureartes9@gmail.com>
2024-09-24 12:01:17 -0300

    [SPT2-152]: fix(header): buttons link and made header text dynamic based on route

--- src/components/common/Header/HeaderBar.tsx
+++ src/components/common/Header/HeaderBar.tsx
@@ -18,7 +18,7 @@ import { searchSchema } from '@/lib/schemas/commonSchemas';
 import { useSession } from 'next-auth/react';
 import { useUserData } from '@/hooks/useUserData';
 import HeaderMenu from './HeaderMenu';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { CartContext, ICartContext } from '@/context/CartContext';
 
 interface HeaderBarProps {
@@ -26,6 +26,28 @@ interface HeaderBarProps {
   setSearchTerm: (value: string) => void;
   setOpenResults: (value: boolean) => void;
   setEnterKeyPress: (value: boolean) => void;
+}
+
+const pathsMap: {[key: string]: string} = {
+  '/': 'Products',
+  '/cart': 'Cart',
+  '/profile/products': 'My Products',
+  '/profile/products/add-product': 'Add Product',
+  '/profile/settings': 'Settings',
+}
+
+const handlePathname = (path: string): string => {
+  const headerText: string = pathsMap[path]
+
+  if (!headerText) {
+    if (/d/.test(path)) {
+      return 'Product'
+    } else {
+      return ''
+    }
+  }  
+
+  return headerText
 }
 
 const HeaderBar = ({
@@ -38,11 +60,15 @@ const HeaderBar = ({
   const { data: userData } = useUserData(session?.user.jwt);
   const router = useRouter();
 
+
+  const pathname: string = usePathname()
+  const headerText: string = handlePathname(pathname)
+ 
   const [isTyping, setIsTyping] = useState(false);
   const [showInputSearch, setShowInputSearch] = useState(false);
-
+  
   const { cartList } = useContext(CartContext) as ICartContext;
-
+ 
   const handleOnChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const targetValue = e.target.value;
     const validation = searchSchema.safeParse(targetValue);
@@ -81,12 +107,15 @@ const HeaderBar = ({
           justifyContent: 'space-between',
         }}
       >
-        <Box
-          sx={{ ml: { xs: '4px', md: '16px' } }}
-          component="img"
-          alt="logo"
-          src="/logo.svg"
-        />
+        <Box sx={{ ml: '12px', mr: '-4px' }}>
+          <Link href='/' style={{ display: 'block', padding: '4px' }}>
+            <Box
+              component="img"
+              alt="logo"
+              src="/logo.svg"
+            />
+          </Link>
+        </Box>
 
         <Typography
           sx={{
@@ -98,7 +127,7 @@ const HeaderBar = ({
             visibility: { xs: 'hidden', md: 'visible' },
           }}
         >
-          Products
+          {headerText}
         </Typography>
 
         {session ? (
@@ -106,6 +135,7 @@ const HeaderBar = ({
         ) : (
           <Button
             variant="outlined"
+            onClick={() => router.push('/auth/sign-in')}
             sx={{
               mr: '40px',
               color: 'secondary.light',
@@ -121,15 +151,7 @@ const HeaderBar = ({
               },
             }}
           >
-            <Link
-              style={{
-                textDecoration: 'none',
-                color: 'inherit',
-              }}
-              href="/auth/sign-in"
-            >
-              Sign in
-            </Link>
+            Sign in
           </Button>
         )}
 
@@ -206,21 +228,23 @@ const HeaderBar = ({
           </IconButton>
 
           {session ? (
-            <Link href="/profile/products">
-              <IconButton
-                aria-label="avatar"
-                sx={{
-                  display: isTyping ? 'none' : { xs: 'none', md: 'block' },
-                  mr: '28px',
-                }}
-              >
-                <Avatar
-                  alt="profileAvatar"
-                  src={userData?.avatar?.url}
-                  sx={{ width: '24px', height: '24px' }}
-                />
-              </IconButton>
-            </Link>
+            <Box 
+              sx={{
+                display: isTyping ? 'none' : { xs: 'none', md: 'block' },
+              }}
+            >
+              <Link href="/profile/products" style={{ marginRight: '28px', display: 'block' }}>
+                <IconButton
+                  aria-label="avatar"
+                >
+                  <Avatar
+                    alt="profileAvatar"
+                    src={userData?.avatar?.url}
+                    sx={{ width: '24px', height: '24px' }}
+                  />
+                </IconButton>
+              </Link>
+            </Box>
           ) : (
             ''
           )}
