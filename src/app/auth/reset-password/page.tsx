b7e865b93cee8b2a4921a8b874176737427efbf3
facureartes <facureartes9@gmail.com>
2024-09-26 20:11:22 -0300

    fix deploy

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,9 +1,11 @@
 import { Box } from '@mui/material';
 import Logo from '@/components/common/Logo';
 import Image from 'next/image';
+import { Suspense } from 'react';
 import ResetPasswordFormContainer from '@/components/auth/resetPassword/ResetPasswordFormContainer';
 import sneakersResetPassword from '@/images/sneakers-reset-password.webp';
 import { Metadata } from 'next';
+import Loading from '@/components/common/Loading';
 
 export const metadata: Metadata = {
   title: 'Reset password',
@@ -12,36 +14,38 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        minWidth: '100vw',
-        minHeight: '100vh',
-      }}
-    >
-      <Logo />
-      <ResetPasswordFormContainer />
-
+    <Suspense fallback={<Loading/>}>
       <Box
         sx={{
-          width: '50%',
-          height: '100vh',
-          m: 0,
-          p: 0,
-          display: { xs: 'none', md: 'block' },
-          position: 'relative',
+          display: 'flex',
+          justifyContent: 'space-between',
+          minWidth: '100vw',
+          minHeight: '100vh',
         }}
       >
-        <Image
-          src={sneakersResetPassword}
-          alt="Sneakers"
-          fill
-          objectFit="cover"
-          priority
-          placeholder="blur"
-        />
+        <Logo />
+        <ResetPasswordFormContainer />
+
+        <Box
+          sx={{
+            width: '50%',
+            height: '100vh',
+            m: 0,
+            p: 0,
+            display: { xs: 'none', md: 'block' },
+            position: 'relative',
+          }}
+        >
+          <Image
+            src={sneakersResetPassword}
+            alt="Sneakers"
+            fill
+            objectFit="cover"
+            priority
+            placeholder="blur"
+          />
+        </Box>
       </Box>
-    </Box>
+    </Suspense>
   );
 }
