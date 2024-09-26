e000fdc78d0e76089f6a8accec658f667dbe607f
facureartes <facureartes9@gmail.com>
2024-09-26 19:33:01 -0300

    [SPT2-151] fix(resetPassword): illegal return error

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@mui/material';
 import Logo from '@/components/common/Logo';
 import Image from 'next/image';
-import { Suspense } from 'react';
 import ResetPasswordFormContainer from '@/components/auth/resetPassword/ResetPasswordFormContainer';
 import sneakersResetPassword from '@/images/sneakers-reset-password.webp';
 import { Metadata } from 'next';
@@ -13,38 +12,36 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <Suspense fallback={''}>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        minWidth: '100vw',
+        minHeight: '100vh',
+      }}
+    >
+      <Logo />
+      <ResetPasswordFormContainer />
+
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          minWidth: '100vw',
-          minHeight: '100vh',
+          width: '50%',
+          height: '100vh',
+          m: 0,
+          p: 0,
+          display: { xs: 'none', md: 'block' },
+          position: 'relative',
         }}
       >
-        <Logo />
-        <ResetPasswordFormContainer />
-
-        <Box
-          sx={{
-            width: '50%',
-            height: '100vh',
-            m: 0,
-            p: 0,
-            display: { xs: 'none', md: 'block' },
-            position: 'relative',
-          }}
-        >
-          <Image
-            src={sneakersResetPassword}
-            alt="Sneakers"
-            fill
-            objectFit="cover"
-            priority
-            placeholder="blur"
-          />
-        </Box>
+        <Image
+          src={sneakersResetPassword}
+          alt="Sneakers"
+          fill
+          objectFit="cover"
+          priority
+          placeholder="blur"
+        />
       </Box>
-    </Suspense>
+    </Box>
   );
 }
