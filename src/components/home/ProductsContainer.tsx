f7b50bb21b40eca3ebe15af32251a54530aa026f
Marcos Abreu <marcosabreudasilva@gmail.com>
2024-10-02 05:29:13 -0300

    [SPT2-177] feat(home-page): empty products state

--- src/components/home/ProductsContainer.tsx
+++ src/components/home/ProductsContainer.tsx
@@ -3,18 +3,15 @@ import ProductCard from './ProductCard';
 import { APIProductsType } from '@/lib/apiDataTypes';
 import { useContext } from 'react';
 import { CartContext, ICartContext } from '@/context/CartContext';
+import EmptyProducts from './EmptyProducts';
 
-interface ProductsContainerProps {
+interface Props {
   searchTerm: string;
   products: APIProductsType[];
 }
 
-export default function ProductsContainer({
-  products,
-}: ProductsContainerProps) {
-
+export default function ProductsContainer({ products }: Props) {
   const { handleAddToCart } = useContext(CartContext) as ICartContext;
-
 
   return (
     <Box
@@ -24,17 +21,20 @@ export default function ProductsContainer({
         justifyContent: 'center',
         flexWrap: 'wrap',
         gap: { xs: '16px', md: '0' },
+        height: '100%',
       }}
     >
-      {Array.isArray(products) && products.length > 0
-        ? products.map(product => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              handleAddToCart={handleAddToCart}
-            />
-          ))
-        : 'No products Found'}
+      {Array.isArray(products) && products.length > 0 ? (
+        products.map(product => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
+        ))
+      ) : (
+        <EmptyProducts />
+      )}
     </Box>
   );
 }
