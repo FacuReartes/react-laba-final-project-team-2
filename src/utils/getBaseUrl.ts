829a7eb69b9bf664347973803f6b5d6a6b595472
facureartes <facureartes9@gmail.com>
2024-10-19 01:45:41 -0300

    [SPT2-202] chore(env): util modification

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -2,6 +2,6 @@ export const getBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return `${process.env.NEXT_PUBLIC_URL}`
   }
-  
-  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  console.log(process.env.VERCEL_URL)
+  return `https://${process.env.VERCEL_URL}`;
 };
