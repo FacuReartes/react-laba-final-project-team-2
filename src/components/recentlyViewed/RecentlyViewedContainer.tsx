81b68e43286a19f697c4b82564e3f251e7891f5a
Lucas Montecino <lucasmontecino95@gmail.com>
2024-10-29 14:56:52 -0300

    [SPT2-207] style(Product Empty State): refactor the code a little bit so the ProductEmptyState looks properly

--- src/components/recentlyViewed/RecentlyViewedContainer.tsx
+++ src/components/recentlyViewed/RecentlyViewedContainer.tsx
@@ -1,103 +1,78 @@
-'use client'
-import { Box, Button, List, ListItem, Typography } from '@mui/material'
+'use client';
+import { Box, List, ListItem, Typography } from '@mui/material';
 import ProductCard from '../common/ProductCard';
 import { useContext } from 'react';
 import { ICartContext, CartContext } from '@/context/cart/CartContext';
 import { useRecentlyViewed } from '@/hooks/useRecentlyViewed';
 import { APIProductsType } from '@/lib/apiDataTypes';
-import { useRouter } from 'next/navigation';
 import Loading from '../common/Loading';
+import ProductsEmptyState from '../common/ProductsEmptyState';
 
 const RecentlyViewedContainer: React.FC = () => {
-
   const { handleAddToCart } = useContext(CartContext) as ICartContext;
 
-  const { productList, isLoading } = useRecentlyViewed()
-
-  const router = useRouter()
+  const { productList, isLoading } = useRecentlyViewed();
 
   return (
-    <Box 
-      sx={{ 
-        width:'100%',
-        px: { xs: '20px', sm: '60px' }, 
+    <Box
+      sx={{
+        width: '100%',
+        px: { xs: '20px', sm: '60px' },
         mt: { xs: '20px', md: '40px' },
       }}
     >
-      <Typography 
-        sx={{ 
-          fontSize: { xs: '30px', sm: '45px' }, 
-          lineHeight: '52.79px', 
-          fontWeight: 500 
+      <Typography
+        sx={{
+          fontSize: { xs: '30px', sm: '45px' },
+          lineHeight: '52.79px',
+          fontWeight: 500,
         }}
       >
         Recently Viewed
       </Typography>
-      { isLoading ? 
-      <Loading/>
-      : 
-      <List 
-        sx={{ 
-          my: { xs: '20px', sm:'40.8px' }, 
-          p: 0,
-          display: 'grid',
-          gridTemplateColumns: {xs: 'repeat(2, 1fr)', lg:'repeat(3, 1fr)', xl:'repeat(4, 1fr)'},
-          columnGap: { xs: '16px', md: '45px', xl: '60px' },
-          rowGap: { xs: '16px', md: '40px' },
-        }} 
-      >
-        { productList.length > 0 ? productList.map((product: APIProductsType) => (
-          <ListItem 
-            sx={{
-              p: 0, 
-              display: 'list-item', 
-              width: 'auto'
-            }} 
+      {isLoading ? (
+        <Loading />
+      ) : productList.length > 0 ? (
+        productList.map((product: APIProductsType) => (
+          <List
             key={product.id}
+            sx={{
+              my: { xs: '20px', sm: '40.8px' },
+              p: 0,
+              display: 'grid',
+              gridTemplateColumns: {
+                xs: 'repeat(2, 1fr)',
+                lg: 'repeat(3, 1fr)',
+                xl: 'repeat(4, 1fr)',
+              },
+              columnGap: { xs: '16px', md: '45px', xl: '60px' },
+              rowGap: { xs: '16px', md: '40px' },
+            }}
           >
-            <ProductCard 
-              product={product} 
-              handleAddToCart={handleAddToCart}
-              upperHeight='338px'
-            />
-          </ListItem>
-        )) : 
-        <Box
-          sx={{
-            my: '50px',
-            textAlign: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: 2,
-            px: { xs: '20px', sm: '0px' },
-          }}
-        >
-        <Typography sx={{ fontWeight: '500', fontSize: '20px' }}>
-          You haven&apos;t watched any products recently.
-        </Typography>
-        <Button
-          onClick={() => router.push('/')}
-          variant="contained"
-          disableElevation
-          size="large"
-          sx={{
-            bgcolor: 'secondary.light',
-            color: 'common.white',
-            height: '40px',
-            transition: 'opacity .2s ease',
-            ':hover': { bgcolor: 'secondary.light', opacity: '.9' },
-            borderRadius: 2,
-          }}
-        >
-          Continue Shopping
-        </Button>
-      </Box>
-        }
-      </List>
-    }
+            <ListItem
+              sx={{
+                p: 0,
+                display: 'list-item',
+                width: 'auto',
+              }}
+            >
+              <ProductCard
+                product={product}
+                handleAddToCart={handleAddToCart}
+                upperHeight="338px"
+              />
+            </ListItem>
+          </List>
+        ))
+      ) : (
+        <ProductsEmptyState
+          buttonText="Continue Shopping"
+          path="/"
+          text="You havent watched any products recently."
+        />
+      )}
     </Box>
-  )
-}
+  );
+};
 
-export default RecentlyViewedContainer
+export default RecentlyViewedContainer;
--- src/components/wishList/ProductsWishList.tsx
+++ src/components/wishList/ProductsWishList.tsx
@@ -14,23 +14,26 @@ export default function ProductsWishList() {
     WishListContext
   ) as IWishListContext;
   return (
-    <Box
-      sx={{
-        my: { xs: '20px', sm: '40.8px' },
-        p: 0,
-        display: 'grid',
-        gridTemplateColumns: {
-          xs: 'repeat(2, 1fr)',
-          lg: 'repeat(3, 1fr)',
-          xl: 'repeat(4, 1fr)',
-        },
-        columnGap: { xs: '16px', md: '45px', xl: '60px' },
-        rowGap: { xs: '16px', md: '40px' },
-      }}
-    >
+    <>
       {wishList.length > 0 ? (
         wishList.map((wish: APIProductsType) => (
-          <ProductWish product={wish} key={wish.id} removeWish={removeWish} />
+          <Box
+            key={wish.id}
+            sx={{
+              my: { xs: '20px', sm: '40.8px' },
+              p: 0,
+              display: 'grid',
+              gridTemplateColumns: {
+                xs: 'repeat(2, 1fr)',
+                lg: 'repeat(3, 1fr)',
+                xl: 'repeat(4, 1fr)',
+              },
+              columnGap: { xs: '16px', md: '45px', xl: '60px' },
+              rowGap: { xs: '16px', md: '40px' },
+            }}
+          >
+            <ProductWish product={wish} removeWish={removeWish} />
+          </Box>
         ))
       ) : (
         <ProductsEmptyState
@@ -39,6 +42,6 @@ export default function ProductsWishList() {
           buttonText="Add Products to Wishlist"
         />
       )}
-    </Box>
+    </>
   );
 }
