0c7dab75f3a95c84e070321dcaac2afc2fa7bb7e
Lucas Montecino <lucasmontecino95@gmail.com>
2024-10-29 16:09:18 -0300

    [SPT2-207] test: update the queryByText because the last change in the Product Empty State

--- src/__tests__/unit/profile/recently-viewed/RecentlyViewed.test.tsx
+++ src/__tests__/unit/profile/recently-viewed/RecentlyViewed.test.tsx
@@ -1,8 +1,8 @@
 import ProductDetail from '@/components/common/ProductDetail/ProductDetail';
-import RecentlyViewedContainer from '@/components/recentlyViewed/RecentlyViewedContainer'
+import RecentlyViewedContainer from '@/components/recentlyViewed/RecentlyViewedContainer';
 import { CartContext } from '@/context/cart/CartContext';
 import { WishListContext } from '@/context/wishlist/WishListContext';
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import { ReactNode } from 'react';
 
 jest.mock('next/navigation', () => ({
@@ -12,15 +12,15 @@ jest.mock('next/navigation', () => ({
     };
   },
   usePathname() {
-    return 'localhost:3000/profile/product/1'
-  }
+    return 'localhost:3000/profile/product/1';
+  },
 }));
 
 jest.mock('@tanstack/react-query', () => ({
   useQuery() {
     return {
-      data: mockProduct[0]
-    }
+      data: mockProduct[0],
+    };
   },
 }));
 
@@ -33,7 +33,7 @@ const renderWithContext = (ui: ReactNode) => {
         removeWish: jest.fn(),
         isSuccess: true,
         handleClose: jest.fn(),
-        message: ''
+        message: '',
       }}
     >
       <CartContext.Provider
@@ -49,120 +49,133 @@ const renderWithContext = (ui: ReactNode) => {
         {ui}
       </CartContext.Provider>
     </WishListContext.Provider>
-  )
-}
+  );
+};
 
 const mockProduct = [
   {
-    'id':99,
-    'attributes': {
-      'name':'test',
-      'description':'test',
-      'price':10,
-      'createdAt':'1',
-      'updatedAt':'1',
-      'publishedAt':'1',
-      'teamName':'team-2',
-      'images': {
-        data: [{
-          id: 1,
-          attributes: {
-            url: '/no-url',
-            name: 'image-name',
-          }
-        }]
+    id: 99,
+    attributes: {
+      name: 'test',
+      description: 'test',
+      price: 10,
+      createdAt: '1',
+      updatedAt: '1',
+      publishedAt: '1',
+      teamName: 'team-2',
+      images: {
+        data: [
+          {
+            id: 1,
+            attributes: {
+              url: '/no-url',
+              name: 'image-name',
+            },
+          },
+        ],
       },
-      'brand':1,
-      'categories':[1],
-      'color':1,
-      'gender': { 
+      brand: 1,
+      categories: [1],
+      color: 1,
+      gender: {
         data: {
           attributes: {
-            name: 'Male'
-          }
-        }
+            name: 'Male',
+          },
+        },
       },
-      'sizes': {
-        data: [{
-          id: 1,
-          attributes: {
-            value: 1
-          }
-        }]
-      }
-    }
-  }
-]
+      sizes: {
+        data: [
+          {
+            id: 1,
+            attributes: {
+              value: 1,
+            },
+          },
+        ],
+      },
+    },
+  },
+];
 
-beforeEach(() => localStorage.clear())
-      
+beforeEach(() => localStorage.clear());
+
 describe('Recently Viewed Tests', () => {
-
   it('Product should be correctly stored in browser storage when viewed', () => {
     jest.spyOn(Storage.prototype, 'setItem');
 
-    renderWithContext(<ProductDetail params={1}/>)
+    renderWithContext(<ProductDetail params={1} />);
 
     // Local storage is called with the product data
-    expect(localStorage.setItem).toHaveBeenCalledWith('recently-watched', JSON.stringify(mockProduct));
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'recently-watched',
+      JSON.stringify(mockProduct)
+    );
+  });
 
-  })
-    
   it('User should be able to see all recently viewed products', () => {
     // Mock a list of 3 products and save them to storage
-    const productList = Array(3).fill('').map((product, index) => {
-      product = {...mockProduct[0]}
-      product.id = index
-      return product
-    })
+    const productList = Array(3)
+      .fill('')
+      .map((product, index) => {
+        product = { ...mockProduct[0] };
+        product.id = index;
+        return product;
+      });
 
-    localStorage.setItem('recently-watched', JSON.stringify(productList))
+    localStorage.setItem('recently-watched', JSON.stringify(productList));
 
-    renderWithContext(<RecentlyViewedContainer/>)
+    renderWithContext(<RecentlyViewedContainer />);
 
     // We get 3 products displayed
-    expect(screen.getAllByRole('product')).toHaveLength(3)
-
-  })
+    expect(screen.getAllByRole('product')).toHaveLength(3);
+  });
 
   it('User should be able to see empty state', () => {
+    renderWithContext(<RecentlyViewedContainer />);
 
-    renderWithContext(<RecentlyViewedContainer/>)
-
-    expect(screen.queryAllByRole('product')).toHaveLength(0)
-    expect(screen.queryByText(/You haven't watched any products recently./i)).toBeInTheDocument()
-
-  })
+    expect(screen.queryAllByRole('product')).toHaveLength(0);
+    expect(
+      screen.queryByText(/You havent watched any products recently./i)
+    ).toBeInTheDocument();
+  });
 
   it('Old products should be deleted when reaching the product limit', () => {
     // Mock a list of 12 (max length) products and save them to storage
-    const productList = Array(12).fill('').map((product, index) => {
-      product = {...mockProduct[0]}
-      product.id = index
-      return product
-    })
+    const productList = Array(12)
+      .fill('')
+      .map((product, index) => {
+        product = { ...mockProduct[0] };
+        product.id = index;
+        return product;
+      });
 
-    localStorage.setItem('recently-watched', JSON.stringify(productList))
+    localStorage.setItem('recently-watched', JSON.stringify(productList));
 
-    expect(JSON.parse(localStorage.getItem('recently-watched')!)).toHaveLength(12)
+    expect(JSON.parse(localStorage.getItem('recently-watched')!)).toHaveLength(
+      12
+    );
 
-    renderWithContext(<ProductDetail params={1}/>)
+    renderWithContext(<ProductDetail params={1} />);
 
-    const newProductList = JSON.parse(localStorage.getItem('recently-watched')!)
+    const newProductList = JSON.parse(
+      localStorage.getItem('recently-watched')!
+    );
     // Length stays the same, and the last item (id = 11) has been erased
-    expect(newProductList).toHaveLength(12)
-    expect(newProductList.find((product: { id: number }) => product.id === 11)).toBeFalsy()
-
-  })
+    expect(newProductList).toHaveLength(12);
+    expect(
+      newProductList.find((product: { id: number }) => product.id === 11)
+    ).toBeFalsy();
+  });
 
   it('Products should not be repeated', () => {
+    localStorage.setItem('recently-watched', JSON.stringify(mockProduct));
 
-    localStorage.setItem('recently-watched', JSON.stringify(mockProduct))
-    
-    renderWithContext(<ProductDetail params={1}/>)
+    renderWithContext(<ProductDetail params={1} />);
 
     // Same product has been filtered out, keeping a length of 1
-    expect(JSON.parse(localStorage.getItem('recently-watched')!)).toHaveLength(1)
-  })
-
-})
+    expect(JSON.parse(localStorage.getItem('recently-watched')!)).toHaveLength(
+      1
+    );
+  });
+});
